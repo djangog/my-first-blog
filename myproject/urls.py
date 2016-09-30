8b37e0b69b25af6ed9c2dfd1b623b404472cd593
Your Name <djangogirl@yandex.ru>
2016-09-30 16:19:10 +0200

    Changed the HTML for the site5.

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,7 +2,7 @@
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
+Examples1:
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
